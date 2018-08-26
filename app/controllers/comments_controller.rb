64023320f66c697b649e93ee2f795c9214397a97
gerson <gersonrevatta@gmail.com>
2018-08-26 17:42:55 -0500

    Add comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,24 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  def create
+    @link = Link.find(params[:link_id])
+    @comment = @link.comments.new(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      redirect_to @link
+    end
+  end
+
+  def destroy
+    @link = Link.find(params[:link_id])
+    @comment = @link.comments.find(params[:id])
+    @comment.destroy
+    redirect_to @link
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:body, :link_id, :user_id)
+  end
 end
+
