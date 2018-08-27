5e4b742e485e98f4c4fb079e47a9adc42ee554e3
gerson <gersonrevatta@gmail.com>
2018-08-27 13:59:43 -0500

    wip

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ApplicationRecord
   has_many :comments
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  def authenticatable_salt
+    "#{super}#{session_token}"
+  end
+
+  def invalidate_session!
+    self.session_token = SecureRandom.hex
+  end
+
 end
