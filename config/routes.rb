ac3277edd0d518e585279c89a3ac084f00af72cd
gerson <gersonrevatta@gmail.com>
2018-09-07 19:13:37 -0500

    Add contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  match '/send_mail', to: 'contacts#send_mail', via: 'post'
   resources :links do
     member do
       put 'like', to: 'links#upvote'
