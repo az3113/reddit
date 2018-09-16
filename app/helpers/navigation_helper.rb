4939b81fc15dfac084e93124da4b923b134e5107
gerson <gersonrevatta@gmail.com>
2018-09-16 16:58:01 -0500

    Refactor navigation helper

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,11 +1,5 @@
 module NavigationHelper
-
   def links_partial_path
-    if user_signed_in?
-      'layouts/shared/header/signed_in_links'
-    else
-      'layouts/shared/header/non_signed_in_links'
-    end
+    user_signed_in? ? 'layouts/shared/header/signed_in_links' : 'layouts/shared/header/non_signed_in_links'
   end
-  
 end
