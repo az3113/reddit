c33bb003014c40b6c99bdd708fd5b18e79ad99b5
gerson <gersonrevatta@gmail.com>
2018-09-16 17:55:32 -0500

    Add link helper

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,2 +1,5 @@
 module LinksHelper
+  def contact_user_partial_path
+    @link.user.id == current_user.id || has_role?(:admin) ? 'links/show/signed_in_links' : 'links/show/non_signed_in_links'
+  end
 end
