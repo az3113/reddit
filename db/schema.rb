ef3ec2533b6f6370be37871ce794a74154fc6488
gerson <gersonrevatta@gmail.com>
2018-10-03 23:11:19 -0500

    Add gems and config

--- db/schema.rb
+++ db/schema.rb
@@ -57,8 +57,6 @@ ActiveRecord::Schema.define(version: 2018_09_16_025239) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.string "login_token"
-    t.datetime "start_session"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
